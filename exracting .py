3cdce88a3fd262e90474ba21380be11772651137
irisxvii <iriskurien@gmail.com>
2024-07-06 10:32:07 +0530

    added url

---
+++ exracting .py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Firefox()  
+
+url = "https://www.example.com/news"  # Replace with the actual website URL
+driver.get(url)
+
+
+
